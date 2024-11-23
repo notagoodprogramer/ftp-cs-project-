5431619d409f14b5d32f1ac27a37e6e5bed36987
avshalom virtual machine <avshalommk@gmail.com>
2024-11-23 13:42:24 +0200

    updated server with 5 users, no option to create new user for now.

--- ftp_first_Server.py
+++ ftp_first_Server.py
@@ -1,38 +1,33 @@
+from pathlib import Path
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
+rootpath = "root" 
+
 def main():
-    # Instantiate a dummy authorizer to handle user authentication
     authorizer = DummyAuthorizer()
+    password = "12345"
 
-    # Add a user with full r/w permissions
-    authorizer.add_user("user", "12345", R"C:\ftp_first_server_filesys", perm="elradfmw")
-    # elradfmw stands for: 
-    # e = change directories (CWD, CDUP commands)
-    # l = list files (LIST, NLST, STAT, MLSD, MLST, SIZE commands)
-    # r = retrieve file from the server (RETR command)
-    # a = append data to an existing file (APPE command)
-    # d = delete file or directory (DELE, RMD commands)
-    # f = rename file or directory (RNFR, RNTO commands)
-    # m = create directory (MKD command)
-    # w = store a file on the server (STOR, STOU commands)
+    #
+    for i in range(5):  
+        user_name = "user" + str(i)
+        user_folder_name = "user" + str(i) + "folder"
+        user_folder = Path(rootpath) / user_folder_name  
+        
+        if user_folder.exists() and user_folder.is_dir():
+            authorizer.add_user(user_name, password, str(user_folder), perm="elradfmw")
+        else:
+            print(f"Directory for {user_name} does not exist: {user_folder}")
 
-    # Allow anonymous connections
-    authorizer.add_anonymous(R"C:\ftp_first_server_filesys")
-
-    # Instantiate an FTP handler
     handler = FTPHandler
     handler.authorizer = authorizer
 
-    # Define a customized banner (string returned when client connects)
     handler.banner = "Welcome to the FTP server."
 
-    # Instantiate the FTP server
-    address = ('0.0.0.0', 2121)  # Listen on all network interfaces at port 2121
+    address = ('0.0.0.0', 2121) 
     server = FTPServer(address, handler)
-
-    # Start the FTP server
+    
     server.serve_forever()
 
 if __name__ == "__main__":
